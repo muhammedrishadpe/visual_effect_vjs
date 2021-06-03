d0cb4745b40c37e0817fc45c3e9d540dad028663
muhammedrishadpe <rishadpe@gmail.com>
2021-06-03 16:13:07 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function scrollAppear(){
     var introText = document.querySelector('.intro-text');
     var introPosition = introText.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight;
+    var screenPosition = window.innerHeight / 1.2;
 
     if(introPosition < screenPosition){
         introText.classList.add('intro-appear');
